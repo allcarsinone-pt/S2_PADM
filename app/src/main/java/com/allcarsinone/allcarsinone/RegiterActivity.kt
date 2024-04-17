77676930229cef093601d296741c246adbb1b4d0
andre <andrerafael112@gmail.com>
2024-04-17 11:57:39 +0100

    created register screen

---
+++ app/src/main/java/com/allcarsinone/allcarsinone/RegiterActivity.kt
@@ -0,0 +1,15 @@
+package com.allcarsinone.allcarsinone
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegiterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_regiter)
+
+    }
+}
