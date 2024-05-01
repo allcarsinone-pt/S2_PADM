653fe862c9fca75bb90a078bbe6f621279a39c71
goncalos10 <a18847@alunos.ipca.pt>
2024-05-01 14:20:54 +0100

    Added ChangePasswordActivity and the XML. Updated XML for recover password button to be in the middle.

--- app/src/main/java/com/allcarsinone/allcarsinone/ChangePasswordActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/ChangePasswordActivity.kt
@@ -6,10 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class RecoverPasswordActivity : AppCompatActivity() {
+class ChangePasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_recover_password)
+        setContentView(R.layout.activity_change_password)
     }
 }
