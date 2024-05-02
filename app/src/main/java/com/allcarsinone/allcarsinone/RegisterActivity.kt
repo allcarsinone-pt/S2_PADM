9e62bbd48128c62d7197f5334a6c03ebf29e897d
goncalos10 <a18847@alunos.ipca.pt>
2024-05-02 17:27:11 +0100

    Added validations on register form.

--- app/src/main/java/com/allcarsinone/allcarsinone/RegisterActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/RegisterActivity.kt
@@ -1,8 +1,11 @@
 package com.allcarsinone.allcarsinone
 
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.allcarsinone.allcarsinone.databinding.ActivityRegisterBinding
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -13,6 +16,25 @@ class RegisterActivity : AppCompatActivity() {
         viewBinding = ActivityRegisterBinding.inflate(layoutInflater)
         val view = viewBinding.root
         setContentView(view)
+        viewBinding.registerRegisterBtn.setOnClickListener {
+            validateDataFields()
+        }
+    }
+
+    fun validateDataFields() {
+        val username = viewBinding.registerUsernameEt.text.toString()
+        val name = viewBinding.registerNameEt.text.toString()
+        val email = viewBinding.registerEmailEt.text.toString()
+        val password = viewBinding.registerPasswordEt.text.toString()
+        val confirmPassword = viewBinding.registerConfirmPasswordEt.text.toString()
+
+        val pattern = Patterns.EMAIL_ADDRESS.matcher(email)
+
+        if(username.length < 5 || password.length < 8 ||
+            confirmPassword != password || !(pattern.matches()))
+            Toast.makeText(this, "Erro", Toast.LENGTH_LONG).show()
+        if(name.length < 5)
+            Toast.makeText(this, "Name must be at least 5 characters.", Toast.LENGTH_LONG).show()
     }
 
 }
