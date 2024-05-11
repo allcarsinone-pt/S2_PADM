10199a2750d66886d5362604fa5331660a79fc9f
andre <andrerafael112@gmail.com>
2024-05-11 16:00:26 +0100

    Created edit vehicle activity

---
+++ app/src/main/java/com/allcarsinone/allcarsinone/EditVehicleActivity.kt
@@ -0,0 +1,13 @@
+package com.allcarsinone.allcarsinone
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class EditVehicleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_edit_vehicle)
+
+    }
+}
