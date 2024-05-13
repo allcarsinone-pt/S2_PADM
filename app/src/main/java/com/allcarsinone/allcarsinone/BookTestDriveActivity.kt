f9335d63cfc445ff42b4e6dbed9af0c473346c8b
goncalos10 <a18847@alunos.ipca.pt>
2024-05-13 12:53:46 +0100

    Add XML layout for BookTestDriveActivity

--- app/src/main/java/com/allcarsinone/allcarsinone/BookTestDriveActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/BookTestDriveActivity.kt
@@ -11,10 +11,7 @@ class BookTestDriveActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_book_test_drive)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
+
+
 }
