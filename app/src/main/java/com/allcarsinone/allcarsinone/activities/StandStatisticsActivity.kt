a2e58f8353f60833e0c9e73798e07adf9fa7693e
pestevao <pestevao@sdilab.pt>
2024-05-16 17:12:41 +0100

    StandsStatistics pie and bar charts implementation

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/StandStatisticsActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/StandStatisticsActivity.kt
@@ -11,23 +11,36 @@ import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
 
+import com.github.mikephil.charting.charts.PieChart
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+
+
+
 class StandStatisticsActivity : AppCompatActivity() {
     lateinit var barChart: BarChart
     lateinit var barData: BarData
     lateinit var barDataSet: BarDataSet
     lateinit var barEntriesList: ArrayList<BarEntry>
 
+    lateinit var pieChart: PieChart
+    lateinit var pieData: PieData
+    lateinit var pieDataSet: PieDataSet
+    lateinit var pieEntriesList: ArrayList<PieEntry>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_stand_statistics)
-        
+
         loadBarChart()
+        loadPieChart()
     }
     fun loadBarChart(){
         barChart = findViewById(R.id.StandStatisticsChart)
         getBarChartData()
-        barDataSet = BarDataSet(barEntriesList, "Chart Data")
+        barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
         barData = BarData(barDataSet)
         barChart.data = barData
         barDataSet.valueTextColor = Color.BLACK
@@ -35,6 +48,19 @@ class StandStatisticsActivity : AppCompatActivity() {
         barDataSet.valueTextSize = 16f
         barChart.description.isEnabled = false
     }
+
+    fun loadPieChart(){
+        pieChart = findViewById(R.id.StandStatisticsPieChart)
+        getPieChartData()
+        pieDataSet = PieDataSet(pieEntriesList, "Pie Chart Data")
+        pieData = PieData(pieDataSet)
+        pieChart.data = pieData
+        pieDataSet.valueTextColor = Color.BLACK
+        pieDataSet.setColor(resources.getColor(R.color.red))
+        pieDataSet.valueTextSize = 16f
+        pieChart.description.isEnabled = false
+    }
+
     private fun getBarChartData() {
         barEntriesList = ArrayList()
         barEntriesList.add(BarEntry(1f, 1f))
@@ -42,5 +68,14 @@ class StandStatisticsActivity : AppCompatActivity() {
         barEntriesList.add(BarEntry(3f, 3f))
         barEntriesList.add(BarEntry(4f, 4f))
         barEntriesList.add(BarEntry(5f, 5f))
+    }
+
+    private fun getPieChartData() {
+        pieEntriesList = ArrayList()
+        pieEntriesList.add(PieEntry(1f, 1f))
+        pieEntriesList.add(PieEntry(2f, 2f))
+        pieEntriesList.add(PieEntry(3f, 3f))
+        pieEntriesList.add(PieEntry(4f, 4f))
+        pieEntriesList.add(PieEntry(5f, 5f))
     }
 }
