11051cb5370b9e2508e13c650cf19d208e29016c
goncalos10 <a18847@alunos.ipca.pt>
2024-05-19 12:09:36 +0100

    Added XML for view stand location activity

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/ViewStandLocationActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/ViewStandLocationActivity.kt
@@ -12,10 +12,5 @@ class ViewStandLocationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_view_stand_location)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
