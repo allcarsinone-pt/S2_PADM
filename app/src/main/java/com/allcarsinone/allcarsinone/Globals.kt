15d68765c27f9e769ba63e95fd2d4160aeccc540
andre <andrerafael112@gmail.com>
2024-05-22 19:40:53 +0100

    TODO for Paulo: host backend on Raspberry PI at some domain

--- app/src/main/java/com/allcarsinone/allcarsinone/Globals.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/Globals.kt
@@ -7,9 +7,11 @@ import retrofit2.create
 
 object Globals {
 
+
     lateinit var userAPI: UserAPI
     private lateinit var retrofit: Retrofit
     fun initAPIs() {
+        // For development: change base url, For production: host at cloud our self host backend
         retrofit = Retrofit.Builder().baseUrl("https://e0a1-193-137-231-142.ngrok-free.app/").addConverterFactory(GsonConverterFactory.create()).build()
         userAPI = retrofit.create(UserAPI::class.java)
 
