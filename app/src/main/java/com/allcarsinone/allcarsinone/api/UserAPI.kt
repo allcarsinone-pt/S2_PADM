b27f7f149faf6c45e1101fde2a71ceae73b30366
goncalos10 <a18847@alunos.ipca.pt>
2024-05-22 17:47:17 +0100

    Added register user to UserAPI

---
+++ app/src/main/java/com/allcarsinone/allcarsinone/api/UserAPI.kt
@@ -0,0 +1,16 @@
+package com.allcarsinone.allcarsinone.api
+
+import com.allcarsinone.allcarsinone.dtos.RegisterUserDto
+import com.allcarsinone.allcarsinone.models.User
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.GET
+import retrofit2.http.PUT
+import retrofit2.http.PATCH
+interface UserAPI {
+
+    @POST("users")
+    fun register(@Body registerUserDto: RegisterUserDto) : Call<User>
+
+}
