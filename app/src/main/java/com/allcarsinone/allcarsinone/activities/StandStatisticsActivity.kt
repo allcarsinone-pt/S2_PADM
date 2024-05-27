4c7f7ca1b4d0de5892407da888a8bc536ba8980f
pestevao <pestevao@sdilab.pt>
2024-05-27 14:05:47 +0100

    Stand Statistics activity - Conclusion

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/StandStatisticsActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/StandStatisticsActivity.kt
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity
 import com.allcarsinone.allcarsinone.R
 
 import android.graphics.Color
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.github.mikephil.charting.charts.BarChart
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
@@ -16,31 +18,46 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 
-
-
 class StandStatisticsActivity : AppCompatActivity() {
-    lateinit var barChart: BarChart
-    lateinit var barData: BarData
-    lateinit var barDataSet: BarDataSet
-    lateinit var barEntriesList: ArrayList<BarEntry>
+    private lateinit var barChart: BarChart
+    private lateinit var barData: BarData
+    private lateinit var barDataSet: BarDataSet
+    private lateinit var barEntriesList: ArrayList<BarEntry>
 
-    lateinit var pieChart: PieChart
-    lateinit var pieData: PieData
-    lateinit var pieDataSet: PieDataSet
-    lateinit var pieEntriesList: ArrayList<PieEntry>
-
+    private lateinit var pieChart: PieChart
+    private lateinit var pieData: PieData
+    private lateinit var pieDataSet: PieDataSet
+    private lateinit var pieEntriesList: ArrayList<PieEntry>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_stand_statistics)
 
+        val spinnerTop = findViewById<Spinner>(R.id.StandsStatisticsTop_Spinner_SP)
+        val spinnerBot = findViewById<Spinner>(R.id.StandsStatisticsBottom_Spinner_SP)
+        val itemsPeriod = arrayOf("Today", "This week", "This month", "Max")
+        val adapterMon = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemsPeriod )
+        spinnerTop.setAdapter(adapterMon)
+        spinnerBot.setAdapter(adapterMon)
+
         loadBarChart()
         loadPieChart()
     }
-    fun loadBarChart(){
+    private fun loadPieChart(){
+        pieChart = findViewById(R.id.StandStatisticsPieChart)
+        getPieChartData()
+        pieDataSet = PieDataSet(pieEntriesList, "Vehicles availability")
+        pieData = PieData(pieDataSet)
+        pieChart.data = pieData
+        pieDataSet.valueTextColor = Color.BLACK
+        pieDataSet.setColors(resources.getColor(R.color.red), resources.getColor(R.color.greenChart))
+        pieDataSet.valueTextSize = 16f
+        pieChart.description.isEnabled = false
+    }
+    private fun loadBarChart(){
         barChart = findViewById(R.id.StandStatisticsChart)
         getBarChartData()
-        barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
+        barDataSet = BarDataSet(barEntriesList, "Vehicles sales")
         barData = BarData(barDataSet)
         barChart.data = barData
         barDataSet.valueTextColor = Color.BLACK
@@ -48,19 +65,11 @@ class StandStatisticsActivity : AppCompatActivity() {
         barDataSet.valueTextSize = 16f
         barChart.description.isEnabled = false
     }
-
-    fun loadPieChart(){
-        pieChart = findViewById(R.id.StandStatisticsPieChart)
-        getPieChartData()
-        pieDataSet = PieDataSet(pieEntriesList, "Pie Chart Data")
-        pieData = PieData(pieDataSet)
-        pieChart.data = pieData
-        pieDataSet.valueTextColor = Color.BLACK
-        pieDataSet.setColor(resources.getColor(R.color.red))
-        pieDataSet.valueTextSize = 16f
-        pieChart.description.isEnabled = false
+    private fun getPieChartData() {
+        pieEntriesList = ArrayList()
+        pieEntriesList.add(PieEntry(1f, 1f))
+        pieEntriesList.add(PieEntry(2f, 2f))
     }
-
     private fun getBarChartData() {
         barEntriesList = ArrayList()
         barEntriesList.add(BarEntry(1f, 1f))
@@ -68,14 +77,5 @@ class StandStatisticsActivity : AppCompatActivity() {
         barEntriesList.add(BarEntry(3f, 3f))
         barEntriesList.add(BarEntry(4f, 4f))
         barEntriesList.add(BarEntry(5f, 5f))
-    }
-
-    private fun getPieChartData() {
-        pieEntriesList = ArrayList()
-        pieEntriesList.add(PieEntry(1f, 1f))
-        pieEntriesList.add(PieEntry(2f, 2f))
-        pieEntriesList.add(PieEntry(3f, 3f))
-        pieEntriesList.add(PieEntry(4f, 4f))
-        pieEntriesList.add(PieEntry(5f, 5f))
     }
 }
