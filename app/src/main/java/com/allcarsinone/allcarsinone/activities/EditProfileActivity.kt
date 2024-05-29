932c9d52a4d9590552e71fdb1b1e4ca485174ad5
goncalos10 <a18847@alunos.ipca.pt>
2024-05-29 10:45:29 +0100

    Craetd a dto for Edit User and use the dto in EditProfileActivity

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/EditProfileActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/EditProfileActivity.kt
@@ -1,5 +1,6 @@
 package com.allcarsinone.allcarsinone.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Patterns
 import android.widget.Toast
@@ -7,11 +8,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.allcarsinone.allcarsinone.Globals
 import com.allcarsinone.allcarsinone.databinding.ActivityEditProfileBinding
+import com.allcarsinone.allcarsinone.dtos.EditUserDto
+import com.allcarsinone.allcarsinone.models.User
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditProfileActivity : AppCompatActivity() {
 
     private lateinit var viewBinding : ActivityEditProfileBinding
+    private val usersAPI by lazy { Globals.userAPI }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityEditProfileBinding.inflate(layoutInflater)
@@ -22,6 +31,33 @@ class EditProfileActivity : AppCompatActivity() {
         }
     }
 
+    fun editUser(body: EditUserDto) {
+        val call = usersAPI.edit(body.username, body)
+
+        call.enqueue(object: Callback<User> {
+            override fun onResponse(p0: Call<User>, p1: Response<User>) {
+                if(p1.isSuccessful) {
+                    Toast.makeText(this@EditProfileActivity,
+                        "User updated with success!",
+                        Toast.LENGTH_LONG).show()
+                }
+                else {
+                    when(p1.code()) {
+                        400 -> {
+                            Toast.makeText(this@EditProfileActivity,
+                                JSONObject(p1.errorBody()?.string()).getString("message"),
+                                Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(p0: Call<User>, p1: Throwable) {
+                Toast.makeText(this@EditProfileActivity, p1.message, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
     fun validateDataFields() {
         val username = viewBinding.editprofileUsernameEt.text.toString()
         val name = viewBinding.editprofileNameEt.text.toString()
@@ -29,17 +65,11 @@ class EditProfileActivity : AppCompatActivity() {
         val password = viewBinding.editprofilePasswordEt.text.toString()
         val confirmPassword = viewBinding.editprofileConfirmPasswordTv.text.toString()
 
-        val pattern = Patterns.EMAIL_ADDRESS.matcher(email)
-
-        if(username.length < 5)
-            Toast.makeText(this, "Username must be at least 5 characters", Toast.LENGTH_SHORT).show()
-        if(password.length < 8)
-            Toast.makeText(this, "Password must be at least 8 characters.", Toast.LENGTH_SHORT).show()
-        if(confirmPassword != password)
-            Toast.makeText(this, "Password and Confirmation Password doesn't match.", Toast.LENGTH_SHORT).show()
-        if(!(pattern.matches()))
-            Toast.makeText(this, "Email is not valid. Ex: example@allcarsinone.pt", Toast.LENGTH_SHORT).show()
-        if(name.length < 5)
-            Toast.makeText(this, "Name must be at least 5 characters.", Toast.LENGTH_SHORT).show()
+        try {
+            val user = EditUserDto(username, name, email, password, confirmPassword)
+            editUser(user)
+        } catch (ex:Exception) {
+            Toast.makeText(this, ex.message, Toast.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/allcarsinone/allcarsinone/dtos/EditUserDto.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/dtos/EditUserDto.kt
@@ -1,4 +1,43 @@
 package com.allcarsinone.allcarsinone.dtos
 
-class EditUserDto {
+import android.util.Patterns
+import android.widget.Toast
+import com.google.gson.annotations.SerializedName
+import okhttp3.Address
+
+data class EditUserDto (@SerializedName("username") val username: String,
+                        @SerializedName("name") val name: String,
+                        @SerializedName("email") val email: String,
+                        @SerializedName("password") val password: String,
+                        @SerializedName("confirmPassword") val confirmPassword: String
+)
+{
+
+    init {
+        if(!validateName(name))
+            throw Exception("Name must be at least 5 characters.")
+        if(!validateEmailPatterns(email))
+            throw Exception("Email is not valid. Ex: example@allcarsinone.pt")
+    }
+
+    fun validateName(name: String?): Boolean {
+        return if(name != null) {
+            name.length > 5
+        } else {
+            false
+        }
+    }
+
+    fun validateEmailPatterns(email: String?): Boolean {
+        val pattern = Patterns.EMAIL_ADDRESS.matcher(email)
+
+        return if(email != null) {
+            pattern.matches()
+        } else {
+            false
+        }
+    }
+
+
+
 }
