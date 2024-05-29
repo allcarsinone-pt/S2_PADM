3bace0ba139f0df65c8d5957973ae2420ec28188
goncalos10 <a18847@alunos.ipca.pt>
2024-05-29 09:55:26 +0100

    Added layout for EditStandProfileActivity

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/EditStandProfileActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/EditStandProfileActivity.kt
@@ -12,10 +12,5 @@ class EditStandProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_edit_stand_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
