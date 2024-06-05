7e786e153dca64cc39d652f96077222151e8d303
goncalos10 <a18847@alunos.ipca.pt>
2024-06-05 21:14:45 +0100

    Updated all fields for vehicle model

--- app/src/main/java/com/allcarsinone/allcarsinone/models/Vehicle.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/models/Vehicle.kt
@@ -3,11 +3,17 @@ package com.allcarsinone.allcarsinone.models
 import com.google.gson.annotations.SerializedName
 
 data class Vehicle (@SerializedName("standid") val standid: Number,
+                    @SerializedName("brandid") val brandid: Number,
+                    @SerializedName("gastypeid") val gastypeid: Number,
                     @SerializedName("model") val model: String,
                     @SerializedName("year") val year: Number,
                     @SerializedName("mileage") val mileage: Float,
                     @SerializedName("price") val price: Float,
+                    @SerializedName("availability") val availability: Boolean,
                     @SerializedName("description") val description: String,
                     @SerializedName("gastypename") val gastypename: String,
-                    @SerializedName("brandname") val brandname: String
+                    @SerializedName("brandname") val brandname: String,
+                    @SerializedName("consume") val consume: Float,
+                    @SerializedName("photos") val photos: String,
+                    @SerializedName("id") val id: Number
     )
