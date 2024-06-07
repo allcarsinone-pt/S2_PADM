1919e7e59190bc9b99ed6b1805979b6af57c2737
pestevao <pestevao@sdilab.pt>
2024-06-07 17:09:34 +0100

    Fixes in initPage and InsertEdit activities

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/InsertEditVehicleActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/InsertEditVehicleActivity.kt
@@ -2,29 +2,23 @@ package com.allcarsinone.allcarsinone.activities
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.allcarsinone.allcarsinone.DataUtils
 import com.allcarsinone.allcarsinone.Globals
 import com.allcarsinone.allcarsinone.databinding.ActivityEditVehicleBinding
-import com.allcarsinone.allcarsinone.dtos.EditVehicleDto
-import com.allcarsinone.allcarsinone.models.User
+import com.allcarsinone.allcarsinone.dtos.InsertEditVehicleDto
 import com.allcarsinone.allcarsinone.models.Vehicle
 import com.google.gson.Gson
-import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import okhttp3.MediaType
 import okhttp3.RequestBody
-import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.http.Header
 
 
-class EditVehicleActivity : AppCompatActivity() {
-
+class InsertEditVehicleActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityEditVehicleBinding
     private val vehicleAPI by lazy { Globals.vehicleAPI }
 
@@ -39,7 +33,7 @@ class EditVehicleActivity : AppCompatActivity() {
         }
     }
 
-    private fun processVehicle(token: String, body: EditVehicleDto) {
+    private fun processVehicle(token: String, body: InsertEditVehicleDto) {
 
         val jsonString = Gson().toJson(body)
         val obj = JsonParser.parseString(jsonString).asJsonObject.toString()
@@ -60,53 +54,26 @@ class EditVehicleActivity : AppCompatActivity() {
                 } else {
                     when (response.code()) {
                         400 -> {
-                            Toast.makeText(this@EditVehicleActivity, response.errorBody().toString(), Toast.LENGTH_LONG).show()
+                            Toast.makeText(this@InsertEditVehicleActivity, response.errorBody().toString(), Toast.LENGTH_LONG).show()
                         }
                         else -> {
-                            Toast.makeText(this@EditVehicleActivity, "Error: ${response.code()}", Toast.LENGTH_LONG).show()
+                            Toast.makeText(this@InsertEditVehicleActivity, "Error: ${response.code()}", Toast.LENGTH_LONG).show()
                         }
                     }
                 }
             }
 
             override fun onFailure(call: Call<Vehicle>, t: Throwable) {
-                //Toast.makeText(this@EditVehicleActivity, t.message, Toast.LENGTH_LONG).show()
-                throw t
+                Toast.makeText(this@InsertEditVehicleActivity, t.message, Toast.LENGTH_LONG).show()
+                //throw t
             }
         })
     }
-
-    /*
-    fun makeApiRequestWithToken(token: String) {
-        val call = Globals.userAPI.validate("Bearer $token")
-
-        call.enqueue(object : Callback<User> {
-            override fun onResponse(call: Call<User>, response: Response<User>) {
-                if (response.isSuccessful) {
-                    val apiResponse = response.body()
-                    println("Success: ${apiResponse?.email}")
-                } else {
-                    println("Request failed with status: ${response.code()}")
-                    Toast.makeText(this@EditVehicleActivity, "Request failed with status: ${response.code()}", Toast.LENGTH_LONG).show()
-                }
-            }
-
-            override fun onFailure(call: Call<User>, t: Throwable) {
-                println("Request failed: ${t.message}")
-                Toast.makeText(this@EditVehicleActivity, "Request failed: ${t.message}", Toast.LENGTH_LONG).show()
-            }
-        })
-    }
-    */
-
 
     private fun validateDataFields() {
-        val sharedPrefs = DataUtils.getSharedPreferences(this@EditVehicleActivity)
+        val sharedPrefs = DataUtils.getSharedPreferences(this@InsertEditVehicleActivity)
         //val token = sharedPrefs.getString("token", "")
         val token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidXNlcm5hbWUiOiJhZG1pbnRlc3RlIiwibmFtZSI6IkFkbWluVGVzdGUiLCJlbWFpbCI6ImFkbWludGVzdGVAYWxsY2Fyc2lub25lLnB0IiwicGFzc3dvcmQiOiIkMmIkMTAkd0ZLOXJNb1YvQmFzcDJUeW13cUY5LkZjNmVmYmxwc0tUNFFoUUprc0QvTHY1a3Z2LnVtdU8iLCJhZGRyZXNzIjoiYWtkc3AiLCJjaXR5IjoiYmFyY2Vsb3MiLCJwb3N0YWxjb2RlIjoiMTAtMiIsIm1vYmlsZXBob25lIjoiMTIzNDU2NzgiLCJwaG90byI6bnVsbCwicm9sZV9pZCI6MSwiaWF0IjoxNzE3NjY2NTEyLCJleHAiOjE3MTc2NzM3MTJ9.jDv9CMiuaq9mtOpWcRJN31DtStCVf7JmvpUm1cKyOB4"
-
-        //makeApiRequestWithToken(token)
-
         if (token.isNullOrEmpty()) {
             Toast.makeText(this, "Token is missing", Toast.LENGTH_LONG).show()
             return
@@ -124,12 +91,12 @@ class EditVehicleActivity : AppCompatActivity() {
         val description = "Bebe como um camelo. Para testar um texto grande."
         val brandname = "Matavelhos"
         val gastypename = ""
-        val photos = arrayOf("F1", "F2")
+        val photos = arrayOf("")
         val consume = 11.3
 
         try {
-            val vehicle = EditVehicleDto(
-                standid, brandid, gastypeid, model, year, mileage, price, availability, description, brandname, gastypename, id, consume
+            val vehicle = InsertEditVehicleDto(
+                standid, brandid, gastypeid, model, year, mileage, price, availability, description, brandname, gastypename, id, photos, consume
             )
             processVehicle(token, vehicle)
         } catch (ex: Exception) {
---
+++ app/src/main/java/com/allcarsinone/allcarsinone/dtos/InsertEditVehicleDto.kt
@@ -0,0 +1,18 @@
+package com.allcarsinone.allcarsinone.dtos
+
+import com.google.gson.annotations.SerializedName
+data class InsertEditVehicleDto (@SerializedName("standid") val standid: Number,
+                                 @SerializedName("brandid") val brandid: Number,
+                                 @SerializedName("gastypeid") val gastypeid: Number,
+                                 @SerializedName("model") val model: String?,
+                                 @SerializedName("year") val year: Number,
+                                 @SerializedName("mileage") val mileage: Number,
+                                 @SerializedName("price") val price: Number,
+                                 @SerializedName("availability") val availability: Boolean,
+                                 @SerializedName("description") val description: String,
+                                 @SerializedName("brandname") val brandname: String,
+                                 @SerializedName("gastypename") val gastypename: String,
+                                 @SerializedName("id") val id: Number,
+                                 @SerializedName("photos") val photos: Array<String>,
+                                 @SerializedName("consume") val consume: Double,
+)
