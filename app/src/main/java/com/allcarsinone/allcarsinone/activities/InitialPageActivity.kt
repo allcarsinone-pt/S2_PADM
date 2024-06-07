5a9e46e6d61a39e36f0e770613b4678c419f2ee0
pestevao <pestevao@sdilab.pt>
2024-06-07 16:24:46 +0100

    LisviewVehicles Code optimization - Finished

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/InitialPageActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/InitialPageActivity.kt
@@ -1,4 +1,3 @@
-
 package com.allcarsinone.allcarsinone.activities
 
 import android.content.Intent
@@ -6,8 +5,6 @@ import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,45 +12,52 @@ import com.allcarsinone.allcarsinone.Globals
 import com.allcarsinone.allcarsinone.R
 import com.allcarsinone.allcarsinone.adapters.ListviewVehiclesAdapter
 import com.allcarsinone.allcarsinone.databinding.ActivityInitialPageBinding
-import com.allcarsinone.allcarsinone.dtos.EditVehicleDto
 import com.allcarsinone.allcarsinone.models.Vehicle
-import com.google.gson.Gson
-import com.google.gson.JsonParser
-import okhttp3.MediaType
-import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.ArrayList
 
-class InitialPageActivity : AppCompatActivity() {
-    private lateinit var viewBinding : ActivityInitialPageBinding
+interface OnVehiclesFetchedListener {
+    fun onVehiclesFetched(vehicleList: ArrayList<Vehicle>)
+}
+
+class InitialPageActivity : AppCompatActivity(), ListviewVehiclesAdapter.OnItemClickListener, OnVehiclesFetchedListener {
+    private lateinit var viewBinding: ActivityInitialPageBinding
     private val vehicleAPI by lazy { Globals.vehicleAPI }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityInitialPageBinding.inflate(layoutInflater)
         val view = viewBinding.root
-        setContentView(R.layout.activity_initial_page)
+        setContentView(view)
 
         val spinnerMon = findViewById<Spinner>(R.id.InitPageLocationSpinner_SP)
         val itemsMon = arrayOf("Aveiro", "Bragança", "Braga", "Coimbra", "Faro", "Funchal", "Guarda", "Lisboa", "Portalegre", "Porto", "Santarém", "Viana do Castelo")
-        val adapterMon = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemsMon )
-        spinnerMon.setAdapter(adapterMon)
+        val adapterMon = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, itemsMon)
+        spinnerMon.adapter = adapterMon
 
-        runVehiclesList()
+        // Initialize RecyclerView
+        val recyclerView = viewBinding.VehiclesListViewViewRecycleView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        runVehiclesList(this)
     }
 
-    private val editProfileContract by lazy {
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == RESULT_OK) {
-                Toast.makeText(this@InitialPageActivity,
-                    "User updated with success!",
-                    Toast.LENGTH_LONG).show()
-            }
+    override fun onVehiclesFetched(vehicleList: ArrayList<Vehicle>) {
+        val recyclerView = viewBinding.VehiclesListViewViewRecycleView
+        val adapter = ListviewVehiclesAdapter(vehicleList, this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onItemClick(vehicle: Vehicle) {
+        val intent = Intent(this, ViewVehicleActivity::class.java).apply {
+            putExtra("idV", vehicle.id)
         }
+        startActivity(intent)
     }
 
-    private fun runVehiclesList() {
+    private fun runVehiclesList(listener: OnVehiclesFetchedListener) {
         val call: Call<List<Vehicle>> = vehicleAPI.getAll()
 
         call.enqueue(object : Callback<List<Vehicle>> {
@@ -61,7 +65,7 @@ class InitialPageActivity : AppCompatActivity() {
                 if (response.isSuccessful) {
                     response.body()?.let { vehicleList ->
                         val list = ArrayList(vehicleList)
-                        setupRecyclerView(list)
+                        listener.onVehiclesFetched(list)
                     }
                 } else {
                     when (response.code()) {
@@ -79,12 +83,5 @@ class InitialPageActivity : AppCompatActivity() {
                 Toast.makeText(this@InitialPageActivity, t.message, Toast.LENGTH_LONG).show()
             }
         })
-    }
-
-    private fun setupRecyclerView(vehicleList: ArrayList<Vehicle>) {
-        val adapter = ListviewVehiclesAdapter(vehicleList)
-        val recyclerView = findViewById<RecyclerView>(R.id.VehiclesListViewView_RecycleView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/allcarsinone/allcarsinone/adapters/ListviewVehiclesAdapter.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/adapters/ListviewVehiclesAdapter.kt
@@ -1,48 +1,54 @@
 
 package com.allcarsinone.allcarsinone.adapters
 
+import android.content.Intent
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.LinearLayout
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.allcarsinone.allcarsinone.Notifications
 import com.allcarsinone.allcarsinone.R
+import com.allcarsinone.allcarsinone.activities.ViewVehicleActivity
+import com.allcarsinone.allcarsinone.databinding.ListviewLayoutVehiclesBinding
 import com.allcarsinone.allcarsinone.models.Vehicle
-import java.text.SimpleDateFormat
+import com.bumptech.glide.Glide
 
-class ListviewVehiclesAdapter(private val list: ArrayList<Vehicle>) : RecyclerView.Adapter<ListviewVehiclesAdapter.ViewHolder>() {
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
-    {
-        val image: ImageView = view.findViewById<ImageView>(R.id.listviewVehiclesImageView)
-        val brand: TextView = view.findViewById<TextView>(R.id.listviewVehiclesCarBrand_TV)
-        val price: TextView = view.findViewById<TextView>(R.id.listviewVehiclesCarPrice_TV)
-        val stand: TextView = view.findViewById<TextView>(R.id.listviewVehiclesStandName_TV)
-        val locat: TextView = view.findViewById<TextView>(R.id.listviewVehiclesLocation_TV)
+class ListviewVehiclesAdapter(
+    private val list: ArrayList<Vehicle>,
+    private val listener: OnItemClickListener
+) : RecyclerView.Adapter<ListviewVehiclesAdapter.ViewHolder>() {
+
+    interface OnItemClickListener {
+        fun onItemClick(vehicle: Vehicle)
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder
-    {
-        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.listview_layout_vehicles, viewGroup, false)
+    inner class ViewHolder(val viewBinding: ListviewLayoutVehiclesBinding) : RecyclerView.ViewHolder(viewBinding.root) {
+        fun bindData(vehicle: Vehicle) {
+            viewBinding.listviewVehiclesCarBrandTV.text = "${vehicle.brandname} - ${vehicle.model}"
+            viewBinding.listviewVehiclesCarPriceTV.text = vehicle.price.toString()
+            viewBinding.listviewVehiclesStandNameTV.text = "Ir buscar o nome"   // TODO: Ir buscar o nome
+            viewBinding.listviewVehiclesLocationTV.text = "Barcelos"            // TODO: Ir buscar a localização
+
+            itemView.setOnClickListener {
+                listener.onItemClick(vehicle)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = ListviewLayoutVehiclesBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int)
-    {
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.bindData(list[position])
+
         val vehicle = list[position]
-        val backStyle = when(position % 2) {
+        val backStyle = when (position % 2) {
             0 -> R.drawable.rectangle_round_corners_20_back
             1 -> R.drawable.rectangle_round_corners_20_white
             else -> R.color.gray
         }
-
-        //viewHolder.image.setImageURI(vehicle.photos[0])
-        viewHolder.brand.text = vehicle.brandname + " - " + vehicle.model
-        viewHolder.price.text = vehicle.price.toString()
-        viewHolder.stand.text = "Ir buscar o nome" // vehicle.st standid.toString() // TODO: Ir buscar o nome
-        viewHolder.locat.text = "Barcelos"
     }
+
     override fun getItemCount() = list.size
 }
