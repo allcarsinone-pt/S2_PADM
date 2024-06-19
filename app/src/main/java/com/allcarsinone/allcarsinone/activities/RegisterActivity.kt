ac38fbdfa123a2228f472d43dbddaf7419f6ceb7
goncalos10 <a18847@alunos.ipca.pt>
2024-06-19 20:14:25 +0100

    Added functionality to back button in RegisterActivity

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/RegisterActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/RegisterActivity.kt
@@ -40,6 +40,11 @@ class RegisterActivity : AppCompatActivity() {
         val adapterRole = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, itemsRole )
         viewBinding.registerRoleSPIN.setAdapter(adapterRole)
 
+        viewBinding.NotificationsBackbuttonBtn.setOnClickListener {
+            val intent = Intent(this, InitialPageActivity::class.java)
+            startActivity(intent)
+        }
+
     }
     private fun registerUserCallback(user: User?, errCode: Int) {
         if(user == null) {
