72311e8add38e29ded960f79b24a00d47fe50276
Paulo Estêvão <pauloestevao@gmail.com>
2024-06-19 21:05:13 +0100

    Navigation fixes

--- app/src/main/java/com/allcarsinone/allcarsinone/activities/InitialPageActivity.kt
+++ app/src/main/java/com/allcarsinone/allcarsinone/activities/InitialPageActivity.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.allcarsinone.allcarsinone.AllCarsInOneApplication
 import com.allcarsinone.allcarsinone.fragments.MenuFragment
 import com.allcarsinone.allcarsinone.AuthUtils
+import com.allcarsinone.allcarsinone.CommentsActivity
 import com.allcarsinone.allcarsinone.DataUtils
 import com.allcarsinone.allcarsinone.Globals
 import com.allcarsinone.allcarsinone.NetworkUtils
@@ -91,14 +92,10 @@ class InitialPageActivity : AppCompatActivity(), ListviewVehiclesAdapter.OnItemC
             val intent = Intent(this, ViewStandLocationActivity::class.java)
             startActivity(intent)
         }
-        
-        /*
         viewBinding.initPageFooterMoreImg.setOnClickListener {
-            val intent = Intent(this, commentsActivity::class.java)
+            val intent = Intent(this, CommentsActivity::class.java)
             startActivity(intent)
         }
-        */
-
     }
 
     override fun onStart() {
